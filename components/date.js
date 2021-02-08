5f23bac2bae7d540d2513a48af7fc9fc08f783c7
masa <iwamasa.0526.to@gmail.com>
2021-02-08 21:03:32 +0900

    first commit

---
+++ components/date.js
@@ -0,0 +1,7 @@
+import { parseISO, format } from "date-fns";
+
+//分割代入
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+}
